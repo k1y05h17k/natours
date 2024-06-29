c92d8e1b7e876cc47594e7907e2dc3e439e348bc
Kiyoshi Takahama <k1y05h17k@gmail.com>
2024-06-29 11:39:38 -0300

    feat: create a new file errorController

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -27,21 +29,10 @@ app.use('/api/v1/users', userRouter);
 
 // Handler all errors of application
 app.all('*',(req, res, next) =>{
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.status = 'fail';
-  err.statusCode = 404;
-
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 // Simple Handler error middleware
-app.use((err, req, res, next)=>{
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  })
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
