d14da13eb2439d022a8fbe3f64240f5c7fa51696
Kiyoshi Takahama <k1y05h17k@gmail.com>
2024-06-28 22:58:29 -0300

    refactor add handler error

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) MIDDLEWARES
+
+
+// MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
@@ -15,17 +17,31 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-// 3) ROUTES
+// ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// Handler all errors of application
+app.all('*',(req, res, next) =>{
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+// Simple Handler error middleware
+app.use((err, req, res, next)=>{
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  })
+});
 
 module.exports = app;
