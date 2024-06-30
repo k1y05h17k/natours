0480e287bd71cb28c18aa3f58cff8819710420b9
Kiyoshi Takahama <k1y05h17k@gmail.com>
2024-06-30 14:40:36 -0300

    refactor: add validatorError

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -70,7 +70,7 @@ module.exports = (err, req, res, next) => {
    
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-    if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+    if (error.name === 'ValidationError')      error = handleValidationErrorDB(error); 
 
     sendErrorProd(error, res);
   }
--- models/tourModel.js
+++ models/tourModel.js
@@ -12,7 +12,7 @@ const tourSchema = new mongoose.Schema({
       trim: true,
       maxlength: [40, 'A tour name must have less or equal then 40 characters'],
       minlength: [10, 'A tour name must have more or equals 10 characters'],
-      validate: [validator.isAlpha,'Tour name must only contain characters ']
+      // validate: [validator.isAlpha,'Tour name must only contain characters ']
     },
     slug: String,
     duration:{
