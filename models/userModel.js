fa477e01075b1a711f0b09277d376cc505a7bd72
Kiyoshi Takahama <k1y05h17k@gmail.com>
2024-07-06 19:31:40 -0300

    refactor: add function to create a new token

--- models/userModel.js
+++ models/userModel.js
@@ -6,31 +6,31 @@ const bcrypt = require('bcryptjs');
 //  name, email, photo, password, passwordConfirm
 
 const userSchema = new mongoose.Schema({
-    name:{
+    name: {
         type: String,
         required: [true, 'Please tell us your name!']
     },
-    email:{
+    email: {
         type: String,
         required: [true, 'Please provide your email'],
         unique: true,
         lowercase: true,
         validate: [validator.isEmail, 'Please provide a valid email']
     },
-    photo:{
+    photo: {
         type: String
     },
-    password:{
+    password: {
         type: String,
         required: [true, 'Please provide a password'],
         minlength: 8
     },
-    passwordConfirm:{
+    passwordConfirm: {
         type: String,
         required: [true, 'Please confirm your password'],
         validate: {
             // This only works on SAVE!!
-            validator: function(el){
+            validator: function (el) {
                 return el === this.password;
             },
             message: 'Passwords are not the sane!'
@@ -38,20 +38,18 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-userSchema.pre('save', async function(next){
+userSchema.pre('save', async function (next) {
     // Only run this function if password was actually modified
-    if(!this.isModified('password')) return next();
+    if (!this.isModified('password')) return next();
 
     // Hash the password with cost of 12
     this.password = await bcrypt.hash(this.password, 12);
 
     // Delete passwordConfirm field
-    this.passwordConfirm  = undefined;
-    
+    this.passwordConfirm = undefined;
+
     next();
 });
-
-
 
 const User = mongoose.model('User', userSchema);
 
