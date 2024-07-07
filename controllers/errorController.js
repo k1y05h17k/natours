27dcc7cef2fe4911f7c38101c669134c5328c4dc
Kiyoshi Takahama <k1y05h17k@gmail.com>
2024-07-07 16:55:31 -0300

    refactor add validation login user

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -57,9 +57,10 @@ module.exports = (err, req, res, next) => {
   console.log("Initial Error State:", err);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-
+  err.message = err.message;
+  
   if (process.env.NODE_ENV === 'development') {
-    let error = JSON.parse(JSON.stringify(err));     
+    let error = Object.create(err);    
     if (error.name === 'CastError') error = handleCastErrorDB(error);    
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);    
     if (error.name === 'ValidationError')      error = handleValidationErrorDB(error);     
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,20 @@
-exports.getAllUsers = (req, res) => {
-    res.status(500).json({
-      status: 'error',
-      message: 'This route is not yet defined!'
+const APIFeatures = require('../utils/apiFeature');
+const catchAsync = require('../utils/catchAsync');
+const User = require('./../models/userModel');
+
+exports.getAllUsers = catchAsync(async(req, res) => {
+    const feature = new APIFeatures(User.find());
+    const users = await feature.query
+
+    res.status(200).json({
+      status: 'success',
+      results: users.length,
+      data:{
+        users
+      }
     });
-  };
+  });
+
   exports.getUser = (req, res) => {
     res.status(500).json({
       status: 'error',
