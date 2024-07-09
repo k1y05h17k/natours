61e627010a68b747410eff97b3dabc2d89e17ad7
Kiyoshi Takahama <k1y05h17k@gmail.com>
2024-07-08 22:28:31 -0300

    refactor: add protect routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
@@ -5,7 +6,7 @@ const AppError = require('./../utils/appError');
 
 
 const signToken = id => {
-    return jwt.sign({id}, process.env.JWT_SECRET, {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRES_IN
     });
 }
@@ -21,35 +22,66 @@ exports.signup = catchAsync(async (req, res, next) => {
     const token = signToken(newUser, _id)
 
     res.status(201).json({
-        status:'success',
+        status: 'success',
         token: token,
-        data:{
+        data: {
             user: newUser
         }
     });
 });
 
-exports.signin = catchAsync( async(req, res, next) =>{
+exports.signin = catchAsync(async (req, res, next) => {
     const { email, password } = req.body
 
     // 1) Check if email and password exist
 
-    if(!email || !password){
-       return next(new AppError('Please provide email and password!', 400));
+    if (!email || !password) {
+        return next(new AppError('Please provide email and password!', 400));
     };
 
     // 2) Check if user exist && password is correct
-    const user = await User.findOne({email: email}).select('+password')
-  
-    if(!user || !(await user.correctPassword(password, user.password))){
+    const user = await User.findOne({ email: email }).select('+password')
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
         return next(new AppError('Incorrect email or password', 401));
     };
 
     // 3) If everything ok, send token to client
     const token = signToken(user._id);
     res.status(200).json({
-        status:'success',
-        token:token,
+        status: 'success',
+        token: token,
         user
-    });    
+    });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+
+    // 1 Getting token and checking of it's there
+    let token;
+
+    if (req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(' ')[1];
+    }
+
+    if (!token) {
+        return next(new AppError('You are not logged in! Please log in to get access.', 401));
+    }
+
+    // 2 Verification token
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+    // 3 Check if user still exists
+    const freshUser = await User.findById(decoded.id);
+    if (!freshUser) {
+        return next(new AppError('The user belonging to this token does no longer exists.', 401));
+    }
+
+    // 4 Check if user changed password after the token was issued
+    if (freshUser.changePasswordAtAfter(decoded.iat)) {
+        return next(new AppError('User recently changed password! Please log in again', 401));
+    }
+    req.user = freshUser;
+    next();
+})
--- controllers/errorController.js
+++ controllers/errorController.js
@@ -20,6 +20,10 @@ const handleValidationErrorDB = err => {
   return new AppError(message, 400);
 };
 
+const handleJWTError = () => new AppError('Invalid token. Please log in again!', 401);
+
+const handleJWTExpiredError = () =>new AppError('Your token has expired! Please log in again!', 401);
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -58,20 +62,26 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
   err.message = err.message;
-  
+
   if (process.env.NODE_ENV === 'development') {
-    let error = Object.create(err);    
-    if (error.name === 'CastError') error = handleCastErrorDB(error);    
-    if (error.code === 11000) error = handleDuplicateFieldsDB(error);    
-    if (error.name === 'ValidationError')      error = handleValidationErrorDB(error);     
-    sendErrorDev(error, res);  
-    
-  } else if (process.env.NODE_ENV === 'production') {
     let error = Object.create(err);
-   
+        
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-    if (error.name === 'ValidationError')      error = handleValidationErrorDB(error); 
+    if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+    if (error.name === 'JsonWebTokenError') error = handleJWTError();
+    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
+
+    sendErrorDev(error, res);
+
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = Object.create(err);
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+    if (error.name === 'JsonWebTokenError') error = handleJWTError();
+    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
     sendErrorProd(error, res);
   }
