54a29b535dd78f69f36bafca0d24b3daa7783423
Kiyoshi Takahama <k1y05h17k@gmail.com>
2024-07-14 19:20:05 -0300

    refactor: add send emails with nodemailer/mailtrap

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,8 @@ const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
-
+const sendEmail = require('./../utils/email');
+const { token } = require('morgan');
 
 const signToken = id => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -105,9 +106,36 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 
     // Generate the random reset token
     const resetToken = user.createPasswordResetToken();
-    await user.save({validateBeforeSave: false});
+    await user.save({ validateBeforeSave: false });
+
     // Send it to user's email
-})
+
+    const resetURL = `${req.protocol}://${'host'}/api/v1/auth/resetPassword/${resetToken}`;
+    const message = `Forgot your password? submit a patch request with your new password and passwordConfirm to
+    : ${resetURL}. \nIf you didn't forget your password, please ignore this email!`;
+    
+    try{
+        await sendEmail({
+            email: user.email,
+            subject: 'Your Password reset token (valid for 10 min',
+            message
+        });
+    
+        res.status(200).json({
+            status: 'success',
+            message: 'Token sent to email'
+        });
+    }catch(err){
+        user.passwordResetToken = undefined;
+        user.passwordResetExpires = undefined;
+        await user.save({validateBeforeSave: false});
+
+        return next(new AppError('There was an error sending the email. Try again later', 500))
+
+    }
+
+
+});
 
 exports.resetPassword = (req, res, next) =>{
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,6 +8,6 @@ router.post('/signup', authController.signup);
 router.post('/signin', authController.signin);
 
 router.post('/forgotPassword', authController.forgotPassword);
-router.post('/resetPassword', authController.resetPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
 
 module.exports = router;   
