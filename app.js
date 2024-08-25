39e882e9e2bfaf299be317893e8bfc031377867a
Kiyoshi Takahama <k1y05h17k@gmail.com>
2024-08-25 15:44:59 -0300

    refactor: add  preventing parameter pollution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -36,10 +37,22 @@ app.use('/api',limiter);
 app.use(express.json({limit:'10kb'}));
 
 // Data sanitization against NOSQL query injection
-app.use(mongoSanitize());
+app.use(mongoSanitize({
+  whitelist: [
+    'duration',
+    'ratingQuantity',
+    'rantingAverage',
+    'maxGroupSize',
+    'difficulty',
+    'price'
+  ]
+}));
 
-//  data sanitization against XSS
+// Data sanitization against XSS
 app.use(xss());
+
+// Prevent Parameter Pollution
+app.use(hpp());
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
